8d3832fd77ce772e2e71282b5d2082ac345afb58
airbreather <airbreather@linux.com>
2017-06-10 15:45:23 -0400

    Add project files.

---
+++ MyDemoLib.Tests/DemoCalcTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace MyDemoLib.Tests
+{
+    public sealed class DemoCalcTests
+    {
+        [Fact]
+        public async Task ComputeThree_ShouldReturn_3()
+        {
+            DemoCalc calc = new DemoCalc();
+
+            int expected = await ReadExpectedAsync();
+            int actual = calc.ComputeThree();
+
+            Assert.Equal(expected, actual);
+        }
+
+        private Task<int> ReadExpectedAsync() => Task.FromResult(3);
+    }
+}
---
+++ MyDemoLib/DemoCalc.cs
@@ -0,0 +1,18 @@
+﻿namespace MyDemoLib
+{
+    public sealed class DemoCalc
+    {
+        public int ComputeThree()
+        {
+#if NET20
+            return new System.Xml.XmlDocument[3].Length;
+#elif NET46
+            return new System.Data.DataTable[3].Length;
+#elif NETSTANDARD1_0
+            return new System.Collections.Generic.HashSet<int> { 1, 2, 3 }.Count;
+#elif NETSTANDARD1_3
+            return new System.IO.FileInfo[3].Length;
+#endif
+        }
+    }
+}
